b197ab80a30cd53d86fe5b21530cba71c1577d8d
Zane Cersovsky <zcersovsky@gmail.com>
2016-10-15 10:55:41 -0500

    Fixed atoi() Fixed atoi() negative test Added atoi negative garbage test

--- stdlib.c
+++ stdlib.c
@@ -1,20 +1,47 @@
 #include "string.h"
 #include "stdint.h"
-#include "stdlib.h"
+#include "stdbool.h"
 #include "stdio.h"
 #include "math.h"
+#include "stdlib.h"
+/*A convenience method to shift a digit into a number*/
+inline int addToNumber(int prevNum, char currDigit) {
+	/*48 is the offset between ASCII character codes and actual numbers*/
+	int currNum = (int)currDigit - 48;
+	return (prevNum*10)+currNum;
+}
 int atoi(const char* src) {
-  size_t i = 0;
-  int ret = 0;
-  for(; src[i] != 0; i++) {
-    if (src[i] > 47 && src[i] < 58) {
-      ret += (src[i]-48)*pow(10, i);
-    }
-  }
-  if (src[0] == '-') {
-    ret *= -1;
-  }
-  return ret;
+	size_t i = 0;
+	int ret = 0;
+	bool inNumber = false;
+	bool isNegative = false;
+	for(; src[i] != 0; i++) {
+		/*before the number is found in the string*/
+		if (inNumber == false) {
+			/*check if there is a leading - sign*/
+			if (src[i] == '-') {
+				inNumber = true;
+				isNegative = true;
+			}
+			/*if not, check if the leading digit has been found*/
+			else if (src[i] >= '0' && src[i] <= '9') {
+				inNumber = true;
+				ret = addToNumber(ret, src[i]);
+			}
+		}
+		/*inside the number, check if the current char is still a digit*/
+		else if (src[i] >= '0' && src[i] <= '9'){
+			ret = addToNumber(ret, src[i]);
+		}
+		/*if the current char is not a digit, the number is over*/
+		else {
+			break;
+		}
+	}
+	if (isNegative) {
+		return ret*-1;
+	}
+	return ret;
 }
 char* itoa(int value, char* str, size_t base) {
   if (value == 0) {
--- tests/stdlib/atoi_negative_garbage.c
+++ tests/stdlib/atoi_negative_garbage.c
@@ -1,7 +1,7 @@
 #include "stdlib.h"
 int main(int argc, char const *argv[]) {
-  char str[] = "-12";
-  if (atoi(str) != -12) {
+  char str[] = "-a12";
+  if (atoi(str) != 0) {
     return 1;
   }
   return 0;
