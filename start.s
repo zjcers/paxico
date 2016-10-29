7d1f2bb47cfc0942a1c23aadfeeb685b77c64f79
Zane Cersovsky <zcersovsky@gmail.com>
2016-10-28 23:17:23 -0500

    Fixed startup code and related tests

--- start.s
+++ start.s
@@ -1,21 +1,28 @@
-.globl _start
-_start:
-  call init_malloc
-  popq %rdi; #pop argc off of the stack
-  movq %rsp, %rsi; #pop argv off of the stack
-  #movq %rcx, %rax; #copy argc to rax
-  #movq 8, %rdx; #set rdx to 8
-  #mulq %rdx; #multiply rax by rdx (argc*8); store in rax
-  #movq %rax, %rdi; #copy the length of the array needed into rdi (first argument)
-  #call malloc; #call malloc to allocated [rdi] bytes of memory
-  #movq %rax, %rdi; #base address of malloc'd block in rax; copy to rdi
-  #movq %rcx, %rax;
-  #argv_loop:
-  #  popq %rdx;
-  #  movq %rdx, (%rdi);
-  #  add $8, %rdi;
-  #  cmp %rax, 0; #if rax == 0
-  #  jne argv_loop; #jump back to top of loop
-  call main;
-  movq %rax, %rdi;
-  call sys_exit
+.text
+  .globl _start
+  _start:
+    call init_malloc
+    popq %r15
+    movq %r15, %rax
+    movq $8, %r10
+    mulq %r10
+    movq %rax, %rdi
+    call malloc
+    movq %rax, %r9
+    argvloop:
+      popq %r8
+      cmpq $0, %r8
+      jz doneargv
+      movq %r8, (%rax)
+      addq $8, %rax
+      jmp argvloop
+    doneargv:
+      addq $8, %rsp
+    movq %r15, %rdi
+    movq %r9, %rsi
+    call main
+    movq %rax, %rdi
+    call sys_exit
+
+.data
+  stuff: .ascii "hello\n\0"
--- tests/start/argc.c
+++ tests/start/argc.c
@@ -1,6 +1,3 @@
-#include "stdio.h"
-#include "stdlib.h"
-#include "string.h"
 int main(int argc, char const *argv[]) {
   return !(argc == 2);
 }
