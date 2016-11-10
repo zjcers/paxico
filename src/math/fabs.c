c4a1ac089cf8a66400ffe6e8ba6adf287df76bc5
Zane Cersovsky <zcersovsky@gmail.com>
2016-11-10 17:08:28 -0600

    Cleaned up fabs()

--- src/math/fabs.c
+++ src/math/fabs.c
@@ -1,14 +1,14 @@
 #include "math.h"
 #ifdef PLATFORM_X86
+/*Computes floating point absolute value*/
 double fabs(double x) {
-	double output;
 	__asm__ volatile ("fldl %0\n"
 										"fabs\n"
 										"fstpl %0\n"
 										: "=m" (x)
 										: "0" (x)
 										: "st");
-	return output;
+	return x;
 }
 #else
 double fabs(double x) {
