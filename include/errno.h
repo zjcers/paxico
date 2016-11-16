1f262255f41ce87e42e7defe7fbf39b738300038
Zane J Cersovsky <zcersovsky@gmail.com>
2016-11-15 22:42:45 -0600

    Added a header declaration for the errno global. For now, the actual .data reservation is made in start.s.

--- include/errno.h
+++ include/errno.h
@@ -36,5 +36,5 @@
 #define EPIPE           32      /* Broken pipe */
 #define EDOM            33      /* Math argument out of domain of func */
 #define ERANGE          34      /* Math result not representable */
-
+extern int errno;
 #endif
