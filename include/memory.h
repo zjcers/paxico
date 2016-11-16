0873b129ba54991abb525c6f460427e877e6878f
Zane J Cersovsky <zcersovsky@gmail.com>
2016-11-15 22:48:35 -0600

    Removed a lot of unnecessary functions for the global scope in 	memory.h

--- include/memory.h
+++ include/memory.h
@@ -1,18 +1,7 @@
 #ifndef _Z_MEMORY_H
 #define _Z_MEMORY_H
-#include "stdint.h"
-typedef struct malloc_node {
-  struct malloc_node* next;
-  unsigned int length;
-  void* data;
-} malloc_node;
-void init_malloc();
-size_t getFreeSpace();
-int checkLeaks();
-void* getOriginalBreak();
-malloc_node* findHoleInList(size_t n);
-malloc_node* findHoleAfterList(size_t n);
-malloc_node* getEndNode();
-void addAfter(malloc_node* front);
-void delAfter(malloc_node* front);
+#include <stdint.h>
+/*Private functions used only by the test suite*/
+size_t _LIBSIMPLEC_get_free_space();
+int _LIBSIMPLEC_check_leaks();
 #endif
