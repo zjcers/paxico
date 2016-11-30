76e3417763e86938be8b1b817640f9660f80e11a
Zane Cersovsky <zcersovsky@gmail.com>
2016-11-29 20:40:19 -0600

    Added one more test to atoi Learned to embrace the hate inside of atoi()

--- src/stdlib/atoi.c
+++ src/stdlib/atoi.c
@@ -11,41 +11,30 @@ inline int addToNumber(int prevNum, char currDigit) {
 int atoi(const char* src) {
 	size_t i = 0;
 	int ret = 0;
-	bool inNumber = false;
-	bool isNegative = false;
+	bool minus = false;
+	for(; src[i] !=0; i++) {
+		if (!(isspace(src[i])))
+			break;
+	}
+	switch (src[i]) {
+	case '-':
+		minus = true;
+	case '+':
+		i++;
+		break;
+	}
 	for(; src[i] != 0; i++) {
-		/*before the number is found in the string*/
-		if (inNumber == false) {
-			switch (src[i]) {
-				/*check if there is a leading - sign*/
-				case '-':
-					inNumber = true;
-					isNegative = true;
-					break;
-				/*if not, check if the leading digit has been found*/
-				case '0' ... '9':
-					inNumber = true;
-					ret = addToNumber(ret, src[i]);
-					break;
-				/*if we get this point and it's not whitespace, bail*/
-				default:
-					if (!isspace(src[i])) {
-						return ret;
-					}
-					break;
-			}
-		}
-		/*inside the number, check if the current char is still a digit*/
-		else if (src[i] >= '0' && src[i] <= '9'){
+		switch (src[i]) {
+		case '0' ... '9':
 			ret = addToNumber(ret, src[i]);
-		}
-		/*if the current char is not a digit, the number is over*/
-		else {
+			break;
+		default:
+			goto atoi_end;
 			break;
 		}
 	}
-	if (isNegative) {
-		return ret*-1;
-	}
-	return ret;
+	atoi_end:
+		if (minus)
+			return -1*ret;
+		return ret;
 }
---
+++ tests/stdlib/atoi_none.c
@@ -0,0 +1,8 @@
+#include <stdlib.h>
+int main(int argc, char const *argv[]) {
+  char str[] = " ";
+  if (atoi(str) != 0) {
+    return 1;
+  }
+  return 0;
+}
